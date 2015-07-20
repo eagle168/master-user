3aa01022cc22971e52e901c8f7e506e9760e2a0f
lihe <darkwarlords@sina.com>
2015-07-20 22:03:20 +0800

    [添加优惠劵数据验证]

--- lib/models/coupon.rb
+++ lib/models/coupon.rb
@@ -4,7 +4,11 @@ class Coupon < ActiveRecord::Base
 	self.table_name_prefix = ''
 	belongs_to :users
 
+	attr_accessor :send_range
+	attr_accessor :user_phone
+
 	validates :denomination, presence: true, length: { maximum: 999 }
 	validates :end_at, presence: true
-
+	validates :user_id, numericality: { only_integer: true }
+	validates :used, inclusion: { in: [true, false] }
 end
