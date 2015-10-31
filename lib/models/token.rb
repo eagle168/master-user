09d8fd19c2e2567eb0380e489b76bb9cb02562d8
lihe <darkwarlords@sina.com>
2015-10-31 17:21:31 +0800

    [user register send coupon]

--- lib/models/token.rb
+++ lib/models/token.rb
@@ -30,6 +30,20 @@ class Token < ActiveRecord::Base
                        # 如果该用户可以推送，则推送信息（有 cid 信息）
                       self.user.send_push(PushMessageHelper::notify_shop_coupons_options("账户信息提示", "您收到一张佰佳商城优惠券。")) if self.user.cid.present?
               end
+              coupon_class_names = ['100golf_20151111_20', '100golf_20151111_50', '100golf_20151111_100']
+              send_count = 0
+              coupon_class_names.each do | name |
+                      coupon_class = CouponClass.where("name = ?", name).where("end_on >= ?", Date.current.strftime("%Y-%m-%d") ).first
+                      if coupon_class.present?
+                              date = (coupon_class.beginning_on + coupon_class.days.days).to_date  if Date.current < coupon_class.beginning_on
+                              date = (Date.current + coupon_class.days.days).to_date if Date.current >= coupon_class.beginning_on
+                              CouponItem.create({user_id: user.id, coupon_class_id: coupon_class.id,
+                                                 expired_on: [date, coupon_class.end_on].min})
+                              send_count += 1
+                      end
+             end 
+             # 如果该用户可以推送，则推送信息（有 cid 信息）
+              self.user.send_push(PushMessageHelper::notify_shop_coupons_options("账户信息提示", "您收到 #{send_count} 张佰佳商城优惠券。")) if send_count>0 and self.user.cid.present?
         end
   end
 end
